c3e93b758953483afce361d00d4ac3037b4b0b85
sloumdrone <brian.michael.evans@gmail.com>
2017-11-09 08:25:49 -0800

    FInished most basic logig (order of op, repeat functions, etc). Basic keybaord control established. Started adding a second pannel for functions and plan to add a history button, the history logic is already in place in the code and can be brought up using the calcHistory variable while in the console.

--- resources/main.js
+++ resources/main.js
@@ -1,88 +1,259 @@
 $(document).ready(initialize);
 
-var currentCalculation;
+var calcArr = [];
+var memory = [];
+var calcHistory = [];
+var canDecimal = true;
+var calcPressed = false;
 
 function initialize(){
-  $('.button').on('click',handleClicks);
-}
-
-function handleClicks(){
-  var clickType = getClickType(this);
-
-  if (currentCalculation){
-    var spaceBeforeNow = currentCalculation[currentCalculation.length-1];
-    if (spaceBeforeNow['type'] === 'op' && clickType === 'op'){
-      spaceBeforeNow['value'] = $(this).text();
-      spaceBeforeNow['precedence'] = getPrecedence($(this).text());
-    } else if (spaceBeforeNow['type'] === 'num' && clickType === 'op'){
-      currentCalculation.push({
-        type: 'op',
-        value: $(this).text(),
-        precedence: getPrecedence($(this).text())
-      });
-    } else if (spaceBeforeNow['type'] === 'op' && clickType === 'num'){
-      currentCalculation.push({
-        type: 'num',
-        value: $(this).text(),
-        precedence: null
-      });
-    } else if (spaceBeforeNow['type'] === 'num' && clickType === 'num') {
-      spaceBeforeNow['value'] += $(this).text();
-    } else if (clickType === 'clear') {
-      currentCalculation = undefined;
-    } else if (clickType === 'calc'){
-      runCalc();
+  $('.num').on('click',handleNumClicks);
+  $('.op').on('click',handleOpClicks);
+  $('.dec').on('click',handleDecClicks);
+  $('.calc').on('click',handleCalc);
+  $('.flip').on('click',handleFlip);
+  $('.clr').on('click',clearCalc);
+  $('.ext').on('click',function(){
+    if ($('.secondary-body').css('visibility') === 'hidden'){
+      $('.secondary-body').css({'visibility':'visible','opacity':'1'});
+    } else {
+      $('.secondary-body').css({'visibility':'hidden','opacity':'0'});
     }
-  } else if (clickType === 'num'){
-    currentCalculation = [];
-    currentCalculation.push({
-      type: 'num',
-      value: $(this).text(),
-      precedence: null
-    });
-  }
-  return currentCalculation;
+  });
+  $('body').on('keypress',handleKeyInput);
 }
 
-function getClickType(clickValue){
-  if ($(clickValue).hasClass('num')){
-    return 'num';
-  } else if ($(clickValue).hasClass('op')) {
-    return 'op';
-  } else if ($(clickValue).hasClass('clr')) {
-    return 'clear';
+function handleNumClicks(){
+  calcPressed = false;
+  if (calcArr[0]){
+    if (!isNaN(calcArr[calcArr.length-1]) || calcArr[calcArr.length-1][calcArr[calcArr.length-1].length-1] === '.'){
+      calcArr[calcArr.length-1] += $(this).text();
+    } else {
+      calcArr.push($(this).text());
+    }
   } else {
-    return 'calc';
+    calcArr.push($(this).text());
   }
+  $('.display').text(calcArr.join(' '));
 }
 
-function getPrecedence(value){
-  if (value === 'x' || value === '/'){
-    return 'md';
-  } else if (value === '+' || value === '-'){
-    return 'as';
-  } else if (value === 'x^y'){
-    return 'e';
-  } else {
-    return false;
+function handleFlip(){
+  if (calcArr[0]){
+      if (!isNaN(calcArr[calcArr.length-1])){
+        calcArr[calcArr.length-1] = Number(calcArr[calcArr.length-1]) * -1;
+      }
   }
+  $('.display').text(calcArr.join(' '));
+}
+
+function handleOpClicks(){
+  calcPressed = false;
+  if (calcArr[0]){
+    if (isNaN(calcArr[calcArr.length-1])){
+      calcArr[calcArr.length-1] = $(this).text();
+    } else {
+      calcArr.push($(this).text());
+    }
+    $('.display').text(calcArr.join(' '));
+  } else {
+    $('.display').text('0');
+  }
+  canDecimal = true;
+
+}
+
+function handleDecClicks(){
+  calcPressed = false;
+  if (!isNaN(calcArr[calcArr.length-1]) && canDecimal){
+    calcArr[calcArr.length-1] += '.';
+    canDecimal = false;
+  } else if (canDecimal){
+    calcArr.push('0.');
+    canDecimal = false;
+  }
+  $('.display').text(calcArr.join(' '));
+}
+
+function handleCalc(){
+  canDecimal = true;
+
+  //Allow for repeat = operation
+  if (!calcPressed){
+    memory = calcArr.slice(calcArr.length - 2,calcArr.length);
+    if (isNaN(memory[memory.length-1])){
+      memory = memory.reverse();
+    }
+    calcPressed = true;
+  } else {
+    calcArr = calcArr.concat(memory);
+  }
+
+  //Allow for terminal operator
+  if (isNaN(calcArr[calcArr.length-1])){
+    calcArr.push(calcArr[calcArr.length-2]);
+  }
+
+  //remove entry operators
+  if(isNaN(calcArr[0])){
+    calcArr.shift();
+  }
+
+  calcHistory.unshift(calcArr.join(' '));
+
+  runCalc()
 }
 
 
 function runCalc(){
   var pos;
   var val;
-  if (currentCalculation.length > 1){
-    if (currentCalculation.indexOf('x^y') >= 0){
-      pos = indexOf('x^y');
-      val = calcExponent(currentCalculation[pos-1],currentCalculation[pos+1]);
-    } else if (indexOf('x') >= 0 or)
-    currentCalculation.splice(pos-1,3,val);
-  }
 
+  //begin recursive calculation
+  if (calcArr.length >= 2){
+    if (calcArr.indexOf('^') >= 0){
+      pos = calcArr.indexOf('^');
+      val = calcExponent(calcArr[pos-1],calcArr[pos+1]);
+    } else if (calcArr.findIndex(checkMultiplyOrDivide) >= 0){
+      pos = calcArr.findIndex(checkMultiplyOrDivide);
+      if (calcArr[pos] === 'x'){
+          val = calcMultiply(calcArr[pos-1],calcArr[pos+1]);
+      } else {
+        val = calcDivide(calcArr[pos-1],calcArr[pos+1]);
+      }
+    } else if (calcArr.findIndex(checkAddOrSubtract) >= 0){
+      pos = calcArr.findIndex(checkAddOrSubtract);
+      if (calcArr[pos] === '+'){
+          val = calcAdd(calcArr[pos-1],calcArr[pos+1]);
+      } else {
+        val = calcSubtract(calcArr[pos-1],calcArr[pos+1]);
+      }
+    }
+    calcArr.splice(pos-1,3,val);
+    runCalc();
+  } else {
+    //return calculation
+
+    //check for infinity and rework as ERR
+    if (calcArr[0] === Infinity){
+      calcArr[0] = 'err';
+    }
+    $('.display').text(calcArr[0]);
+    return calcArr[0];
+  }
+}
+
+function clearCalc(){
+  canDecimal = true;
+  calcPressed = false;
+  if ($(this).text() === 'AC'){
+    calcArr = [];
+    $('.display').text('0');
+  } else {
+    calcArr.pop();
+    if (calcArr.length >= 1) {
+        $('.display').text(calcArr.join(' '));
+    } else {
+      $('.display').text('0');
+    }
+
+  }
+}
+
+function checkMultiplyOrDivide(element){
+  return element === '/' || element === 'x';
+}
+
+function checkAddOrSubtract(element){
+  return element === '+' || element === '-';
+}
+
+function calcExponent(num1,num2){
+  return Math.pow(num1,num2);
+}
+
+function calcMultiply(num1,num2){
+  return num1 * num2;
+}
+
+function calcDivide(num1,num2){
+  return num1 / num2;
+}
+
+function calcAdd(num1,num2){
+  return Number(num1) + Number(num2);
+}
+
+function calcSubtract(num1,num2){
+  return num1 - num2;
 }
 
 
-function calcExponent(num1,num2,opLocation){
-  Math.pow(num1,num2);
+function handleKeyInput(){
+  switch (event.charCode){
+    case 65: // AC - 'A'
+    case 97: // AC - 'a'
+      $('.calc-body .row:nth-child(2) .clr:first-child').click();
+      break;
+    case 67: // C - 'C'
+    case 99: // C - 'c'
+      $('.calc-body .row:nth-child(2) .clr:nth-child(2)').click();
+      break;
+    case 94: // ^
+      $('.secondary-body .row:last-child .op:first-child').click();
+      break;
+    case 47: // /
+      $('.calc-body .row:nth-child(2) .op:nth-child(4)').click();
+      break;
+    case 55: // 7
+      $('.calc-body .row:nth-child(3) .num:first-child').click();
+      break;
+    case 56: // 8
+      $('.calc-body .row:nth-child(3) .num:nth-child(2)').click();
+      break;
+    case 57: // 9
+      $('.calc-body .row:nth-child(3) .num:nth-child(3)').click();
+      break;
+    case 88: // X
+    case 120: // x X *
+    case 42: // x X *
+      $('.calc-body .row:nth-child(3) .op').click();
+      break;
+    case 52: // 4
+      $('.calc-body .row:nth-child(4) .num:first-child').click();
+      break;
+    case 53: // 5
+      $('.calc-body .row:nth-child(4) .num:nth-child(2)').click();
+      break;
+    case 54: // 6
+      $('.calc-body .row:nth-child(4) .num:nth-child(3)').click();
+      break;
+    case 43: // +
+      $('.calc-body .row:nth-child(4) .op').click();
+      break;
+    case 49: // 1
+      $('.calc-body .row:nth-child(5) .num:first-child').click();
+      break;
+    case 50: // 2
+      $('.calc-body .row:nth-child(5) .num:nth-child(2)').click();
+      break;
+    case 51: // 3
+      $('.calc-body .row:nth-child(5) .num:nth-child(3)').click();
+      break;
+    case 45: // -
+      $('.calc-body .row:nth-child(5) .op').click();
+      break;
+    case 60: // +/- - '<'
+      $('.calc-body .row:nth-child(6) .flip').click();
+      break;
+    case 48: // 0
+      $('.calc-body .row:nth-child(6) .num').click();
+      break;
+    case 46: // .
+      $('.calc-body .row:nth-child(6) .dec').click();
+      break;
+    case 13: // =
+    case 61: // =
+      $('.calc-body .row:nth-child(6) .calc').click();
+      break;
+  }
 }
